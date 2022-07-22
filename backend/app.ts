49a591783a9287a05b5b30e76138ac251255f91f
sarahtell <sarah.lm.tell@gmail.com>
2022-07-22 14:21:01 +0200

    adds /elevators route

--- backend/app.ts
+++ backend/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { getRandomFloors } from './src/utils';
 
 const app = express();
 const port = process.env.PORT;
@@ -6,3 +7,8 @@ const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`Elevator application is running on port ${port}.`);
 });
+
+app.get('/elevators', (req, res) => {
+    const randomFloors = getRandomFloors(5, 20)
+    res.send({initialLocations: randomFloors})
+  })
