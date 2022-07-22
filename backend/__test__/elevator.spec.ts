7e94a39b1e7be71b0f2992494b0b39c8154a6c29
sarahtell <sarah.lm.tell@gmail.com>
2022-07-22 13:40:47 +0200

    renames function

--- backend/__test__/elevator.spec.ts
+++ backend/__test__/elevator.spec.ts
@@ -1,11 +1,11 @@
-import {getRandomElevatorFloors} from '../src/utils'
+import {getRandomFloors} from '../src/utils'
 
 describe('check elevator functions', () => {
 
     it('is a list with five elements', () => {
         const floors = 20
         const elevators = 5
-        const randomFloors = getRandomElevatorFloors(elevators, floors)
+        const randomFloors = getRandomFloors(elevators, floors)
         console.log(randomFloors)
         expect(randomFloors.length).toBe(5)
     })
--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,4 +1,4 @@
-export function getRandomElevatorFloors(elevators: number, floors: number): number[] {
+export function getRandomFloors(elevators: number, floors: number): number[] {
 
     return [...Array(elevators)].map(_=>Math.round(Math.random()*floors))
 } 
