2514756f413e43137c3ba35dff999bb167cbbfdd
sarahtell <sarah.lm.tell@gmail.com>
2022-07-23 21:19:50 +0200

    adds animation of  elevator

--- frontend/src/components/ElevatorShaft.tsx
+++ frontend/src/components/ElevatorShaft.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 type ElevatorShaftProps = {
   floorFrom: number;
   floorTo: number;
@@ -8,6 +10,16 @@ const NUMBER_OF_FLOORS = 20;
 // In percentage of parent element
 const ELEVATOR_HEIGHT = 4;
 const ELEVATOR_WIDTH = 80;
+const DISTANCE_BETWEEN_FLOORS =
+  (100 - NUMBER_OF_FLOORS * ELEVATOR_HEIGHT) / (NUMBER_OF_FLOORS + 1);
+
+function calculateFloorPositionInPercentage(floor: number) {
+  const floorsFromTop = NUMBER_OF_FLOORS - floor;
+  return (
+    floorsFromTop * (ELEVATOR_HEIGHT + DISTANCE_BETWEEN_FLOORS) +
+    DISTANCE_BETWEEN_FLOORS
+  );
+}
 
 export default function ElevatorShaft(props: ElevatorShaftProps): JSX.Element {
   return (
@@ -26,7 +38,9 @@ export default function ElevatorShaft(props: ElevatorShaftProps): JSX.Element {
           />
         );
       })}
-      <li
+      <motion.li
+        initial={{ top: `${calculateFloorPositionInPercentage(props.floorFrom)}%`}}
+        animate={{top: `${calculateFloorPositionInPercentage(props.floorTo)}%`}}
         style={{
           height: `${ELEVATOR_HEIGHT}%`,
           width: `${ELEVATOR_WIDTH}%`,
