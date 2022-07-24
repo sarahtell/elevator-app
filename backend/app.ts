2fb05f805242b45e77de6bf2b9a6f009e1681585
sarahtell <sarah.lm.tell@gmail.com>
2022-07-24 17:33:26 +0200

    connects frontend and backend

--- backend/app.ts
+++ backend/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import { getRandomFloors } from './src/utils';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(cors())
 
 app.listen(port, () => {
     console.log(`Elevator application is running on port ${port}.`);
--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,4 +1,4 @@
 export function getRandomFloors(elevators: number, floors: number): number[] {
 
-    return [...Array(elevators)].map(_=>Math.round(Math.random()*floors))
+    return [...Array(elevators)].map(_=>Math.round(Math.random()*(floors - 1)))
 } 
