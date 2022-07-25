d864088cc22c4ac9fd097c67ffac4c0ff70d35ff
sarahtell <sarah.lm.tell@gmail.com>
2022-07-25 23:09:32 +0200

    updates tests

--- backend/__test__/elevator.spec.ts
+++ backend/__test__/elevator.spec.ts
@@ -1,11 +1,39 @@
-import { getRandomFloors } from '../src/utils';
+import ElevatorManager from "../src/elevator-manager";
+import { getRandomFloors } from "../src/utils";
 
-describe('check elevator functions', () => {
+describe("check elevator functions", () => {
+  const elevatorManager = new ElevatorManager();
 
-    it('is a list with five elements', () => {
-        const floors = 20
-        const elevators = 5
-        const randomFloors = getRandomFloors(elevators, floors)
-        expect(randomFloors.length).toBe(5)
-    })
-})
+  it("is a list with five elements", () => {
+    const floors = 20;
+    const shafts = 5;
+    const randomFloors = getRandomFloors(shafts, floors);
+    expect(randomFloors.length).toBe(5);
+  });
+
+  it("initializes the class", () => {
+    const floors = 20;
+    const shafts = 5;
+    elevatorManager.initialize(shafts, floors);
+    expect(elevatorManager.shafts.length).toBe(5);
+  });
+
+  it("sends the elevator from closest shaft on button click", () => {
+    elevatorManager.shafts = [
+      {
+        from: 10,
+        to: 10,
+      },
+      {
+        from: 3,
+        to: 3,
+      },
+      {
+        from: 20,
+        to: 20,
+      },
+    ];
+    const closestShaft = elevatorManager.getClosestElevator(2);
+    expect(closestShaft).toBe(1);
+  });
+});
