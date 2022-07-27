94daa68e8ce3cb27f51789c25bc700736cb1d503
sarahtell <sarah.lm.tell@gmail.com>
2022-07-27 15:38:39 +0200

    adds Modal component

---
+++ frontend/src/components/Modal.tsx
@@ -0,0 +1,47 @@
+type ModalProps = {
+  errorMessage: string;
+  setHasError: (state: boolean) => void;
+};
+
+export default function Modal(props: ModalProps): JSX.Element {
+  return (
+    <>
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+          {/*content*/}
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            {/*header*/}
+            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+              <h3 className="text-3xl font-semibold">Error</h3>
+              <button
+                className="p-1 ml-auto bg-transparent border-0 text-black opacity-50 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                onClick={() => props.setHasError(false)}
+              >
+                <span className="bg-transparent text-black h-6 w-6 text-2xl block outline-none focus:outline-none">
+                  ×
+                </span>
+              </button>
+            </div>
+            {/*body*/}
+            <div className="relative p-6 flex-auto">
+              <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                {props.errorMessage}
+              </p>
+            </div>
+            {/*footer*/}
+            <div className="flex items-center justify-end p-6 rounded-b">
+              <button
+                className="bg-sky-400 text-white active:bg-sky-900 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={() => props.setHasError(false)}
+              >
+                OK
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+    </>
+  );
+}
